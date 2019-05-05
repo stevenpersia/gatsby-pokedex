06c23f001a63a497c4ffaf43588f3b6d0ff89ae2
Steven Persia <persia.steven8@gmail.com>
2019-05-05 15:02:49 +0200

    Config gatsby-transformer-json package

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,18 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `POKEDEX GENERATION 1`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-transformer-json`,
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "data",
+        path: `${__dirname}/src/data/`,
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
