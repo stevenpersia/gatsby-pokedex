a98149cee4d21c6da896e594d10b5d2f20238033
Steven Persia <persia.steven8@gmail.com>
2019-05-05 16:46:19 +0200

    Change some configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `POKEDEX GENERATION 1`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `POKEDEX`,
+    description: `Generation 1`,
+    author: `Steven Persia`,
   },
   plugins: [
     `gatsby-transformer-json`,
